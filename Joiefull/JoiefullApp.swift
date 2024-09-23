111fca9881c1b6706435f56f7a0219ba4183e907
i-Louise <louisecindy.ta@gmail.com>
2024-09-23 14:24:52 +0200

    Initial Commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Louise Ta on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
