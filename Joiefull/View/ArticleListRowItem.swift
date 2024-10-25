e2708daee76c5ee27e7ddaae0f10f10e63ea2cca
i-Louise <louisecindy.ta@gmail.com>
2024-10-25 18:13:21 +0200

    Minor indentation fix

--- Joiefull/View/ArticleListRowItem.swift
+++ Joiefull/View/ArticleListRowItem.swift
@@ -13,73 +13,69 @@ struct ArticleListRowItem: View {
     @State private var selectedArticle: Article?
     
     var body: some View {
-        
-            VStack(alignment: .leading) {
-                ZStack(alignment: .bottomTrailing) {
-                    
-                    AsyncImage(url: URL(string: article.picture.url)) { phase in
-                        
-                        switch phase {
-                        case .failure:
-                            Image(systemName: "photo")
-                                .font(.largeTitle)
-                        case .success(let image):
-                            image
-                                .resizable()
-                                .scaledToFill()
-                        default:
-                            ProgressView()
-                        }
+        VStack(alignment: .leading) {
+            ZStack(alignment: .bottomTrailing) {
+                AsyncImage(url: URL(string: article.picture.url)) { phase in
+                    switch phase {
+                    case .failure:
+                        Image(systemName: "photo")
+                            .font(.largeTitle)
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    default:
+                        ProgressView()
                     }
-                    .frame(width: 200, height: 200)
-                    .clipShape(.rect(cornerRadius: 16))
-                    .accessibilityHint(Text("\(article.picture.description)"))
-                    
-                    HStack(spacing: 4) {
-                        Image(systemName: "heart")
-                            .font(.body)
-                        Text("\(article.likes)")
-                            .font(.body)
-                    }
-                    .padding(6)
-                    .background(Color.white)
-                    .cornerRadius(30)
-                    .padding([.bottom, .trailing], 8)
                 }
+                .frame(width: 200, height: 200)
+                .clipShape(.rect(cornerRadius: 16))
+                .accessibilityHint(Text("\(article.picture.description)"))
                 
-                VStack {
-                    HStack {
-                        Text(article.name)
-                            .font(.body)
-                            .foregroundStyle(.black)
-                            .frame(width: 150, alignment: .leading)
-                            .lineLimit(1)
-                            .truncationMode(.tail)
-                        Spacer()
-                        HStack(spacing: 2) {
-                            Image(systemName: "star.fill")
-                                .foregroundColor(.orange)
-                            Text("\(viewModel.rating)")
-                                .foregroundColor(.gray)
-                                .font(.body)
-                            
-                        }
-                    }
-                    HStack {
-                        Text("\(String(format: "%.2f", article.price))€")
-                            .font(.body)
-                            .foregroundColor(.black)
-                        Spacer()
-                        Text("\(String(format: "%.2f", article.originalPrice))€")
-                            .strikethrough()
+                HStack(spacing: 4) {
+                    Image(systemName: "heart")
+                        .font(.body)
+                    Text("\(article.likes)")
+                        .font(.body)
+                }
+                .padding(6)
+                .background(Color.white)
+                .cornerRadius(30)
+                .padding([.bottom, .trailing], 8)
+            }
+            
+            VStack {
+                HStack {
+                    Text(article.name)
+                        .font(.body)
+                        .foregroundStyle(.black)
+                        .frame(width: 150, alignment: .leading)
+                        .lineLimit(1)
+                        .truncationMode(.tail)
+                    Spacer()
+                    HStack(spacing: 2) {
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.orange)
+                        Text("\(viewModel.rating)")
                             .foregroundColor(.gray)
                             .font(.body)
+                        
                     }
                 }
+                HStack {
+                    Text("\(String(format: "%.2f", article.price))€")
+                        .font(.body)
+                        .foregroundColor(.black)
+                    Spacer()
+                    Text("\(String(format: "%.2f", article.originalPrice))€")
+                        .strikethrough()
+                        .foregroundColor(.gray)
+                        .font(.body)
+                }
             }
+        }
 
-            .padding()
-        
+        .padding()
     }
 }
 
