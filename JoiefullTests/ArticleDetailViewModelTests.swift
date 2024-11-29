51b15846e322f037c9fd54583638b4e43099ea77
i-Louise <louisecindy.ta@gmail.com>
2024-11-29 13:08:18 +0100

    Corrected unit test DetailViewModel

--- JoiefullTests/ArticleDetailViewModelTests.swift
+++ JoiefullTests/ArticleDetailViewModelTests.swift
@@ -25,7 +25,7 @@ final class ArticleDetailViewModelTests: XCTestCase {
         picture: Article.Picture(url: "https://raw.githubusercontent.com/OpenClassrooms-Student-Center/Cr-ez-une-interface-dynamique-et-accessible-avec-SwiftUI/main/img/accessories/1.jpg", description: "Sac à main orange posé sur une poignée de porte")
     )
     private let fakeLike = Like(likes: 40, isLiked: false)
-
+    
     // Given no average rating from reviews, When initialized, Then ensure average rating is nil
     func test_init_noAverageRating() {
         // Given
@@ -53,15 +53,16 @@ final class ArticleDetailViewModelTests: XCTestCase {
         // Then
         XCTAssertEqual(underTest.averageRating, 4.2)
     }
-
+    
     // Given a user rating equal to 0, When adding a review, Then ensure the error message is set and alert is shown and no review is stored
     func test_userRatingEqualToZeroErrorMessage() {
         // Given
         let underTest = ArticleDetailViewModel(article: fakeArticle, like: fakeLike, reviewRepository: reviewRepositoryMock)
-        underTest.userRating = 0
+        let userRating = 0
+        let userComment = "No comment"
         
         // When
-        underTest.addReview()
+        underTest.addReview(userRating: userRating, userComment: userComment, onSuccess: {})
         
         // Then
         XCTAssertEqual(underTest.alertMessage, "Veuillez renseigner une notation")
